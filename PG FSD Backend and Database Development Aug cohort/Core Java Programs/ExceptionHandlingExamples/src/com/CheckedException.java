6930a30077824691086d8c3151abfcccdd20508f
Akash <akash300383@gmail.com>
2023-11-04 21:16:01 +0530

    done

---
+++ PG FSD Backend and Database Development Aug cohort/Core Java Programs/ExceptionHandlingExamples/src/com/CheckedException.java
@@ -0,0 +1,14 @@
+package com;
+
+public class CheckedException {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hi");
+
+			//try {
+			Thread.sleep(3000);
+			//}catch(Exception e) {}
+		System.out.println("Take tea break....");
+	}
+
+}
