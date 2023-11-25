5d70420a34c3a8cc8c7371643d0466a1fc2f637f
akash <akash300383@gmail.com>
2023-11-25 19:52:21 +0530

    done

--- PG FSD Backend and Database Development Aug cohort/Core Java Programs/JDBCTestApp/src/com/DemoTest.java
+++ PG FSD Backend and Database Development Aug cohort/Core Java Programs/JDBCTestApp/src/com/DemoTest.java
@@ -7,6 +7,8 @@ public class DemoTest {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("driver loaded successfully");
+Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/my_db_info", "root","root@123");
+System.out.println("connected successfully");
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
