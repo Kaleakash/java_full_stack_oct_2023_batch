879ebb5d592188f4f10aac2f6baf33083845f167
akash <akash300383@gmail.com>
2023-12-09 22:48:37 +0530

    done

---
+++ PG FSD Backend and Database Development Aug cohort/java ee programs/ScopeObject/src/main/java/com/LoginServlet.java
@@ -0,0 +1,52 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("In Servlet Page");
+		String name = request.getParameter("name");
+	pw.println("name is "+name);
+	request.setAttribute("obj", name);
+		RequestDispatcher rd = request.getRequestDispatcher("Home.jsp");
+		//rd.include(request, response);
+		
+		response.setContentType("text/html");
+		rd.forward(request, response);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
