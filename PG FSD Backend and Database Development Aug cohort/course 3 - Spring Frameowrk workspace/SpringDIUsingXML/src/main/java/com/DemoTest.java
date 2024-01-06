79ba85ef86478923c18a6e1757eda6ec59e677d0
akash <akash300383@gmail.com>
2024-01-06 21:11:25 +0530

    done

--- PG FSD Backend and Database Development Aug cohort/course 3 - Spring Frameowrk workspace/SpringDIUsingXML/src/main/java/com/DemoTest.java
+++ PG FSD Backend and Database Development Aug cohort/course 3 - Spring Frameowrk workspace/SpringDIUsingXML/src/main/java/com/DemoTest.java
@@ -1,12 +1,20 @@
 package com;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class DemoTest {
 
 	public static void main(String[] args) {
 		//Employee emp = new Employee();
 		//emp.display();
 
-			
+	Resource res = new ClassPathResource("beans.xml");	// file loaded...
+	BeanFactory obj = new XmlBeanFactory(res);    // it use to get the reference of bean factory 
+	Employee e1 = (Employee)obj.getBean("emp1");   // pull the object. 
+	e1.display();
 	}
 
 }
