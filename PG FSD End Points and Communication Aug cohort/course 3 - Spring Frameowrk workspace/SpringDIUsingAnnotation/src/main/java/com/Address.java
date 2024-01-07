b5be0643dd49a9d4a4b189a58763b93a0a4efccb
akash <akash300383@gmail.com>
2024-01-07 19:57:32 +0530

    done

---
+++ PG FSD End Points and Communication Aug cohort/course 3 - Spring Frameowrk workspace/SpringDIUsingAnnotation/src/main/java/com/Address.java
@@ -0,0 +1,29 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component							//<bean class="com.Address"></bean> id name is address 
+public class Address {
+@Value(value = "Bagalore")
+private String city;
+@Value(value = "Kar")
+private String state;
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+@Override
+public String toString() {
+	return "Address [city=" + city + ", state=" + state + "]";
+}
+
+}
