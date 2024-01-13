337c8c8c0dda1986d3db2f64fa75f5e807bb402e
akash <akash300383@gmail.com>
2024-01-13 19:51:22 +0530

    done

---
+++ PG FSD End Points and Communication Aug cohort/course 3 - Spring Frameowrk workspace/SpringFrameworkWithJdbcTemplate/src/main/java/com/dao/EmployeeDao.java
@@ -0,0 +1,50 @@
+package com.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bean.Employee;
+
+@Repository				// dao layer specific annotation 
+public class EmployeeDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;			// byType 
+	
+	public int storeEmployee(Employee employee) {
+		try {
+// 1st parameter query and 2nd parameter var arguments. it is use to do DML Operation 
+return jdbcTemplate.update("insert into employee values(?,?,?)", employee.getId(),employee.getName(),employee.getSalary());		
+			
+		} catch (Exception e) {
+		System.out.println(e);
+		return 0;
+		}
+	}
+	
+	public int deleteEmployee(int id) {
+		try {
+// 1st parameter query and 2nd parameter var arguments. it is use to do DML Operation 
+return jdbcTemplate.update("delete from employee where id = ?", id);				
+		} catch (Exception e) {
+		System.out.println(e);
+		return 0;
+		}
+	}
+	
+	public int updateEmployee(Employee employee) {
+		try {
+// 1st parameter query and 2nd parameter var arguments. it is use to do DML Operation 
+return jdbcTemplate.update("update employee set salary = ? where id = ?",employee.getSalary(),employee.getId());		
+		} catch (Exception e) {
+		System.out.println(e);
+		return 0;
+		}
+	}
+}
+
+
+
+
+
