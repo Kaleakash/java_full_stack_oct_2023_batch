d0c9c2ae6fdaa98ff01ed79a32f3fdd0b675fecd
akash <akash300383@gmail.com>
2024-01-14 19:45:57 +0530

    done

--- PG FSD End Points and Communication Aug cohort/ORM Workspace/HibernateCRUDOperation/src/main/java/com/main/DemoTest.java
+++ PG FSD End Points and Communication Aug cohort/ORM Workspace/HibernateCRUDOperation/src/main/java/com/main/DemoTest.java
@@ -13,23 +13,38 @@ public class DemoTest {
 	// Hibernate API 
 	// This class load the hibernate.cfg.xml file 
 	Configuration con = new Configuration();
-	con.configure("hibernate.cfg.xml");
+	con.configure("hibernate.cfg.xml");			// Load the xml file 
 	
 	System.out.println("file load successfully");
+	
 	SessionFactory sf = con.buildSessionFactory();		// like Connection in JDBC. 
 	
 	Session session = sf.openSession(); 		// like Statement or PreparedStatement 
 	
 	Transaction tran = session.getTransaction();		// get transaction object. 
-	Employee emp1 = new Employee();
-	emp1.setId(105);
-	emp1.setName("Dinesh");
-	emp1.setSalary(45000);
 	
+	// Insert Query or Save Employee Record 
+//	Employee emp1 = new Employee();
+//	emp1.setId(105);
+//	emp1.setName("Dinesh");
+//	emp1.setSalary(45000);
+//	
+//		tran.begin();
+//			session.save(emp1);					// like insert Query 
+//		tran.commit();
+//		System.out.println("Record inserted successfully...");
+	
+	// Delete Query 
+	Employee emp = session.find(Employee.class, 100); // find method 1st parameter class reference and 2nd value of pk 
+	if(emp==null) {
+		System.out.println("Record not present");
+	}else {
 		tran.begin();
-			session.save(emp1);					// like insert Query 
+		session.delete(emp);		// delete from employee where id = 100;
 		tran.commit();
-		System.out.println("Record inserted successfully...");
+		System.out.println("Record deleted successfully...");
+	}
+	
 	}
 
 }
