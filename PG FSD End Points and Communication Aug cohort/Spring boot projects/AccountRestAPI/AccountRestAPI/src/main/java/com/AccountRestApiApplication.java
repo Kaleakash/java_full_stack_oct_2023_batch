c9877628201cb3e896c887291068fde43a0265be
akash <akash300383@gmail.com>
2024-02-03 22:26:34 +0530

    done

--- PG FSD End Points and Communication Aug cohort/Spring boot projects/AccountRestAPI/AccountRestAPI/src/main/java/com/AccountRestApiApplication.java
+++ PG FSD End Points and Communication Aug cohort/Spring boot projects/AccountRestAPI/AccountRestAPI/src/main/java/com/AccountRestApiApplication.java
@@ -2,12 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")// Enable @restcontroller, @service, @repository 
+@EntityScan(basePackages = "com.entity")
+@EnableJpaRepositories(basePackages = "com.repository")
 public class AccountRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountRestApiApplication.class, args);
+		System.err.println("spring boot up on port number 9090");
 	}
 
 }
