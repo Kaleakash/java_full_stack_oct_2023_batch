ba5de2e211215d92d1bac0c13a68b15f1ae19ae8
akash <akash300383@gmail.com>
2024-02-18 22:48:32 +0530

    done

--- e-shopping-app/frontend/shopping-app/src/app/signup/signup.component.ts
+++ e-shopping-app/frontend/shopping-app/src/app/signup/signup.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-
-}
+export class SignupComponent implements OnInit{
+ 
+  loginRef = new FormGroup({
+    emailid:new FormControl(),
+    password:new FormControl(),
+    typeofuser:new FormControl()
+   });
+  
+    constructor(public ls:LoginService,public router:Router) {}  // di for login service 
+  
+    ngOnInit(): void {
+        
+    }
+    msg:string ="";
+    signUp():void {
+      let login  = this.loginRef.value;
+  
+      console.log(login);
+      this.ls.signUp(login).subscribe({
+        next:(result:any)=> {
+          if(result=="Admin login successfully"){
+            this.msg = result;
+             this.router.navigate(["adminhome"]); 
+          }else if(result=="Customer login successfully"){
+            this.msg = result;
+            this.router.navigate(["customerhome"]); 
+          }else {
+              this.msg = result;
+          }
+        },
+        error:(error:any)=>console.log(error),
+        complete:()=>console.log("signup done!")
+      });
+  
+      this.loginRef.reset();
+    }
+  }
